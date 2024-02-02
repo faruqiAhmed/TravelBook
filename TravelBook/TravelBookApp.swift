e6fd9984e5fb6fc622ad3248d985414f95d6f586
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-02-03 00:48:51 +0600

    Initial commit

---
+++ TravelBook/TravelBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBookApp.swift
+//  TravelBook
+//
+//  Created by Md Omar Faruq on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
